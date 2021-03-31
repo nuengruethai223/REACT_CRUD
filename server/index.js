8adcb3809bde147972bc639e7fe60fb09aad7141
nuengruethai <nuengruethai.kw3456@gmail.com>
2021-03-31 10:06:49 +0700

    add react repository

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+// cors มาจาก corss origin resource sharing เป็นการอนุญาต/ไม่อนุญาต การขอใช้ทรัพยากร
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "admin",
+  host: "localhost",
+  password: "admin",
+  database: "registering",
+});
+
+app.get("/registering", (req, res) => {
+  db.query("SELECT * FROM registering", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen('3001',() => {
+    console.log('server is running on port 3001');
+
+})
